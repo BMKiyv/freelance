6df53827e85df309412c0ed1fbf8282ec0895ae4
BMKiyv <k1brusenets@gmail.com>
2020-09-30 13:31:49 +0300

    second right issue

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   const [work, setWork] = useState('good');
-  const onClick = setWork({work:"bad"});
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
             We will create DForce app
-          <button onClick = {onClick}>{work}</button> and save to reload.
+           and save to reload.
         </p>
-        <a
+        <button onClick = {()=>setWork(work=>work==='good'?'bad':'good')} >{work}</button>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
